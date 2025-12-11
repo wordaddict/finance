6d91812f3d410e2e38d8f80bd2827786fe027a82
Micheal Adeyinka <michealadeyinka@macbookpro.myfiosgateway.com>
2025-12-11 18:53:41 -0500

    add more ctagories

--- src/components/expense-form.tsx
+++ src/components/expense-form.tsx
@@ -605,7 +605,7 @@ export function ExpenseForm({ user, onClose, onSuccess, onCancel, editExpense, n
                   }}
                   className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                 />
-                <span className="text-sm font-medium">This is an event</span>
+                <span className="text-base font-medium">This is an event</span>
               </label>
             </div>
 
