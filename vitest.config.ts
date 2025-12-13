a59231c92a1d7269e119f100f53b470964320aa7
Micheal Adeyinka <michealadeyinka@macbookpro.myfiosgateway.com>
2025-12-12 21:38:50 -0500

    remove vi test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,3 @@
-import { defineConfig } from 'vitest/config'
-import path from 'path'
-
-export default defineConfig({
-  test: {
-    environment: 'node',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
+// Vitest config removed (not in use)
 
 
